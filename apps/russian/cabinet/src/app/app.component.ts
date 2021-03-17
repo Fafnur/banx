0b6f5dd3a61a2dcab45c1c9eba6e4a3cf1dd2756
Aleksandr Serenko <fafnur@yandex.ru>
2021-03-17 08:37:06 +0700

    initial commit

---
+++ apps/russian/cabinet/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'banx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'russian-cabinet';
+}
