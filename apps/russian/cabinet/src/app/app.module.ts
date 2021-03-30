5acbf83d2732b3a38b03d178407516125205fe88
Aleksandr Serenko <fafnur@yandex.ru>
2021-03-30 08:00:37 +0700

    Set locale for russian cabinet

--- apps/russian/cabinet/src/app/app.module.ts
+++ apps/russian/cabinet/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppLocaleModule } from './app-locale.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppLocaleModule],
   providers: [],
   bootstrap: [AppComponent],
 })
