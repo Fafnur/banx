9f9622902cd7ecb13887dc619d1d5cf5ea1aef5c
Aleksandr Serenko <fafnur@yandex.ru>
2021-03-30 23:51:13 +0700

    Add base ui header

---
+++ libs/russian/ui/layout/src/lib/components/ui-header/ui-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'banx-ui-header',
+  templateUrl: './ui-header.component.html',
+  styleUrls: ['./ui-header.component.scss'],
+})
+export class UiHeaderComponent {}
--- libs/russian/ui/layout/src/lib/ui-layout.module.ts
+++ libs/russian/ui/layout/src/lib/ui-layout.module.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
+import { UiContainerModule } from '@banx/russian/ui/container';
+
+import { UiHeaderComponent } from './components/ui-header/ui-header.component';
 import { UiLayoutComponent } from './components/ui-layout/ui-layout.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
-  declarations: [UiLayoutComponent],
+  imports: [CommonModule, RouterModule, MatIconModule, MatButtonModule, UiContainerModule],
+  declarations: [UiLayoutComponent, UiHeaderComponent],
   exports: [UiLayoutComponent],
 })
 export class UiLayoutModule {}
