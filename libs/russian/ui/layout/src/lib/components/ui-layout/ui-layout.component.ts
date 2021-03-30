d54bf3af7e768a8cb9d9e7298c6186556eccf823
Aleksandr Serenko <fafnur@yandex.ru>
2021-03-30 08:44:27 +0700

    Generate ui layout component

---
+++ libs/russian/ui/layout/src/lib/components/ui-layout/ui-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'banx-ui-layout',
+  templateUrl: './ui-layout.component.html',
+  styleUrls: ['./ui-layout.component.scss']
+})
+export class UiLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/russian/ui/layout/src/lib/ui-layout.module.ts
+++ libs/russian/ui/layout/src/lib/ui-layout.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { UiLayoutComponent } from './components/ui-layout/ui-layout.component';
+
 @NgModule({
   imports: [CommonModule],
+  declarations: [UiLayoutComponent],
+  exports: [UiLayoutComponent],
 })
 export class UiLayoutModule {}
