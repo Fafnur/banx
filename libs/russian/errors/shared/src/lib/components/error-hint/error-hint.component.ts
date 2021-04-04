78ed712e784cca5d06dad24a63a8a804e97c4a74
Aleksandr Serenko <fafnur@yandex.ru>
2021-04-04 19:25:56 +0300

    Move error-hint component

--- libs/russian/errors/shared/src/lib/components/error-hint/error-hint.component.ts
+++ libs/russian/errors/shared/src/lib/components/error-hint/error-hint.component.ts
@@ -1,16 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'banx-error-hint',
   templateUrl: './error-hint.component.html',
   styleUrls: ['./error-hint.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ErrorHintComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ErrorHintComponent {}
