1a1771a0a9ed05afebaeacbe864feedc87294f09
Aleksandr Serenko <fafnur@yandex.ru>
2021-05-18 23:03:09 +0700

    Add grid module and components

--- libs/russian/ui/grid/src/index.ts
+++ libs/russian/ui/grid/src/index.ts
@@ -1,1 +1,3 @@
-export * from './lib/russian-ui-grid.module';
+export * from './lib/column/column.component';
+export * from './lib/grid.module';
+export * from './lib/row/row.component';
---
+++ libs/russian/ui/grid/src/lib/grid.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { ColumnComponent } from './column/column.component';
+import { RowComponent } from './row/row.component';
+
+const components = [RowComponent, ColumnComponent];
+@NgModule({
+  declarations: [...components],
+  exports: [...components],
+})
+export class GridModule {}
