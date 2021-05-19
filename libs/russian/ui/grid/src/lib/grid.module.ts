24878605c12d71704974c104079d60cbccd2ce67
Aleksandr Serenko <fafnur@yandex.ru>
2021-05-19 08:47:21 +0700

    Add sample for row columns

--- libs/russian/ui/grid/src/lib/grid.module.ts
+++ libs/russian/ui/grid/src/lib/grid.module.ts
@@ -4,6 +4,7 @@ import { ColumnComponent } from './column/column.component';
 import { RowComponent } from './row/row.component';
 
 const components = [RowComponent, ColumnComponent];
+
 @NgModule({
   declarations: [...components],
   exports: [...components],
