a0bc4964b26066d94042fa34d48c47f77a7d37b0
Aleksandr Serenko <fafnur@yandex.ru>
2021-07-23 08:14:07 +0700

    [Common][Refactor] Change environments

--- libs/core/environments/service/src/lib/environment.interface.ts
+++ libs/core/environments/service/src/lib/environment.interface.ts
@@ -2,6 +2,7 @@ import { InjectionToken } from '@angular/core';
 
 export interface Environments {
   production: boolean;
+  brand: string;
 }
 
 export const ENVIRONMENTS = new InjectionToken<Environments>('Environments');
--- libs/core/environments/service/src/lib/environment.service.ts
+++ libs/core/environments/service/src/lib/environment.service.ts
@@ -2,17 +2,18 @@ import { Inject, Injectable, Optional } from '@angular/core';
 
 import { ENVIRONMENTS, Environments } from './environment.interface';
 
-const ENVIRONMENTS_DEFAULT: Environments = {
+export const ENVIRONMENTS_DEFAULT: Environments = {
   production: false,
+  brand: 'Banx',
 };
 
 @Injectable({
   providedIn: 'root',
 })
 export class EnvironmentService {
-  constructor(@Optional() @Inject(ENVIRONMENTS) private readonly environments: Environments) {}
+  readonly environments: Environments;
 
-  getEnvironments(): Environments {
-    return this.environments ?? ENVIRONMENTS_DEFAULT;
+  constructor(@Optional() @Inject(ENVIRONMENTS) environments: Environments | null) {
+    this.environments = environments ?? ENVIRONMENTS_DEFAULT;
   }
 }
