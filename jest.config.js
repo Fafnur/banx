ffbd781979792e53c22058b791925039cd9326a2
Aleksandr Serenko <fafnur@yandex.ru>
2021-07-25 00:08:52 +0700

    [Common][Vendors] Update vendors

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/russian/cabinet',
-    '<rootDir>/libs/ui/layout',
-    '<rootDir>/libs/ui/container',
-    '<rootDir>/libs/russian/content/home/page',
-    '<rootDir>/libs/errors/not-found/page',
-    '<rootDir>/libs/errors/shared',
-    '<rootDir>/libs/errors/server-error/page',
-    '<rootDir>/libs/errors/permission-denied/page',
-    '<rootDir>/libs/core/storage/common',
-    '<rootDir>/libs/core/storage/memory',
-    '<rootDir>/libs/core/storage/local',
-    '<rootDir>/libs/core/storage/session',
-    '<rootDir>/libs/core/cookie/service',
-    '<rootDir>/libs/core/environments/service',
-    '<rootDir>/libs/core/config/service',
-    '<rootDir>/libs/core/api/service',
-    '<rootDir>/libs/core/api/interceptors/content-type',
-    '<rootDir>/libs/core/api/interceptors/with-credentials',
-    '<rootDir>/libs/core/store/root',
-    '<rootDir>/libs/core/store/utils',
-    '<rootDir>/libs/core/store/watcher',
-    '<rootDir>/libs/ui/grid',
-    '<rootDir>/libs/core/platform/service',
-    '<rootDir>/libs/russian/numerals/common',
-    '<rootDir>/libs/russian/numerals/shared',
-    '<rootDir>/libs/numbers-to-words/common',
-    '<rootDir>/libs/numbers-to-words/shared',
-    '<rootDir>/libs/core/testing',
-    '<rootDir>/libs/russian/localization',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
