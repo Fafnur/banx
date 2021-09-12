89a6f50c0d4abb851303c2708a08c6d2055b1d9f
Aleksandr Serenko <fafnur@yandex.ru>
2021-09-12 11:57:08 +0700

    [Common][Api] Add content type to requests

--- apps/russian/cabinet/src/app/app-core.module.ts
+++ apps/russian/cabinet/src/app/app-core.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AuthStateModule } from '@banx/auth/state';
+import { ApiContentTypeModule } from '@banx/core/api/interceptors/content-type';
+import { ApiWithCredentialsModule } from '@banx/core/api/interceptors/with-credentials';
 import { ENVIRONMENTS } from '@banx/core/environments/service';
 import { MENU, MENU_DEFAULT } from '@banx/core/menu/common';
 import { PATHS } from '@banx/core/navigation/common';
@@ -20,6 +22,8 @@ import { environment } from '../environments/environment';
     LayoutModule,
     LocalizationModule,
     AuthStateModule,
+    ApiContentTypeModule,
+    ApiWithCredentialsModule,
     RootStoreModule,
     !environment.production ? StoreDevtoolsModule.instrument({ logOnly: environment.production }) : [],
   ],
