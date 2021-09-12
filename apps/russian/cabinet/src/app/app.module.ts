12324c295f9047b14947573a5fb4fc351bf203c8
Aleksandr Serenko <fafnur@yandex.ru>
2021-09-12 11:57:46 +0700

    [Common][Animation] Add material animation for UI

--- apps/russian/cabinet/src/app/app.module.ts
+++ apps/russian/cabinet/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppCoreModule } from './app-core.module';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, AppCoreModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppCoreModule, AppRoutingModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
