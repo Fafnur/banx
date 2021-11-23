8c2e5c7ac5decca4baa1a78d8fd86b8f44ec6606
Aleksandr Serenko <fafnur@yandex.ru>
2021-11-23 09:41:04 +0700

    [Russian][Registration] Adding extractTouched directive

---
+++ libs/core/forms/shared/src/lib/extract-touched.directive.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectorRef, Directive, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { takeUntil, tap } from 'rxjs/operators';
+
+import { extractTouchedChanges } from '@banx/core/forms/utils';
+import { DestroyService } from '@banx/core/services';
+
+@Directive({
+  // eslint-disable-next-line @angular-eslint/directive-selector
+  selector: '[extractTouched]',
+  providers: [DestroyService],
+})
+export class ExtractTouchedDirective implements OnInit {
+  @Input() control?: FormControl;
+
+  constructor(private readonly changeDetectorRef: ChangeDetectorRef, private readonly destroy$: DestroyService) {}
+
+  ngOnInit(): void {
+    if (this.control) {
+      extractTouchedChanges(this.control)
+        .pipe(
+          tap(() => this.changeDetectorRef.markForCheck()),
+          takeUntil(this.destroy$)
+        )
+        .subscribe();
+    } else {
+      console.warn('Control is not found on extract touched');
+    }
+  }
+}
