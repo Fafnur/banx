8933cf3338746b21ac313749e78799855856a2d7
Aleksandr Serenko <fafnur@yandex.ru>
2021-11-25 00:04:59 +0700

    [Core][Utils] Adding date utils

--- libs/core/utils/src/index.ts
+++ libs/core/utils/src/index.ts
@@ -1,2 +1,3 @@
+export * from './lib/date.util';
 export * from './lib/interface.util';
 export * from './lib/uuid.util';
---
+++ libs/core/utils/src/lib/date.util.ts
@@ -0,0 +1,24 @@
+export function getMinDate(age: number = 100): Date {
+  const minDate = new Date();
+  minDate.setFullYear(minDate.getFullYear() - age);
+
+  return minDate;
+}
+
+export function getMaxDate(age: number = 18): Date {
+  const maxDate = new Date();
+  maxDate.setFullYear(maxDate.getFullYear() - age);
+
+  return maxDate;
+}
+
+export function getMaskDate(value: string, separate: string = '.'): string {
+  const date = new Date(value);
+  const res = {
+    day: date.getDate(),
+    month: date.getMonth() + 1,
+    year: date.getFullYear(),
+  };
+
+  return `${res.day.toString().padStart(2, '0')}${separate}${res.month.toString().padStart(2, '0')}${separate}${res.year}`;
+}
