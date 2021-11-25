dcc1883355fcd8cda17c95715e52fdecb3d14977
Aleksandr Serenko <fafnur@yandex.ru>
2021-11-25 09:20:24 +0700

    [UI][Global] Set default styles for mat radio buttons

--- libs/russian/localization/src/lib/localization.module.ts
+++ libs/russian/localization/src/lib/localization.module.ts
@@ -2,6 +2,7 @@ import { registerLocaleData } from '@angular/common';
 import localeRu from '@angular/common/locales/ru';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { DateAdapter, MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
 
 import { RussianMatDataAdapter } from './mat-data-adapter';
 
@@ -25,6 +26,10 @@ registerLocaleData(localeRu);
     {
       provide: DateAdapter,
       useClass: RussianMatDataAdapter,
+    },
+    {
+      provide: MAT_RADIO_DEFAULT_OPTIONS,
+      useValue: { color: 'primary' },
     },
   ],
 })
