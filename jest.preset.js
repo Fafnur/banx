62a957eed712d7b9a73027851ebd0f5bb0c18fb3
Aleksandr Serenko <fafnur@yandex.ru>
2021-12-05 00:04:46 +0700

    [Backend][Db] Changing docker compose env

--- jest.preset.js
+++ jest.preset.js
@@ -1,6 +1,3 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = {
-  ...nxPreset,
-  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
-};
+module.exports = { ...nxPreset };
