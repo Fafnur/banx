0e35c2d8850694cf53e8615e809af30bab4f960f
Aleksandr Serenko <fafnur@yandex.ru>
2022-01-03 22:43:55 +0700

    [Registration][Tests] Adding registration social tests

--- libs/russian/registration/social/page/src/lib/registration-social-page.component.spec.ts
+++ libs/russian/registration/social/page/src/lib/registration-social-page.component.spec.ts
@@ -1,25 +1,70 @@
+import { CommonModule } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockModule } from 'ng-mocks';
+import { ReplaySubject } from 'rxjs';
+import { mock, when } from 'ts-mockito';
+
+import { providerOf } from '@banx/core/testing';
+import { RegistrationSocialFacade } from '@banx/registration/social/state';
+import { RegistrationCardModule } from '@banx/registration/ui/card';
+import { RegistrationStepErrorModule } from '@banx/russian/registration/ui/step-error';
+import { SpinnerModule } from '@banx/ui/spinner';
 
 import { RegistrationSocialPageComponent } from './registration-social-page.component';
+import { RegistrationSocialPageComponentPo } from './registration-social-page.component.po';
 
 describe('RegistrationSocialPageComponent', () => {
-  let component: RegistrationSocialPageComponent;
+  let pageObject: RegistrationSocialPageComponentPo;
   let fixture: ComponentFixture<RegistrationSocialPageComponent>;
+  let registrationSocialFacadeMock: RegistrationSocialFacade;
+  let finishSocialFailure$: ReplaySubject<HttpErrorResponse>;
+
+  beforeEach(() => {
+    registrationSocialFacadeMock = mock(RegistrationSocialFacade);
+    finishSocialFailure$ = new ReplaySubject<HttpErrorResponse>(1);
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        NoopAnimationsModule,
+        RouterTestingModule,
+        SpinnerModule,
+        MockModule(MatButtonModule),
+        MockModule(RegistrationCardModule),
+        MockModule(RegistrationStepErrorModule),
+      ],
       declarations: [RegistrationSocialPageComponent],
+      providers: [providerOf(RegistrationSocialFacade, registrationSocialFacadeMock)],
     }).compileComponents();
   });
 
   beforeEach(() => {
+    when(registrationSocialFacadeMock.finishSocialFailure$).thenReturn(finishSocialFailure$);
+
     fixture = TestBed.createComponent(RegistrationSocialPageComponent);
-    component = fixture.componentInstance;
+    pageObject = new RegistrationSocialPageComponentPo(fixture);
   });
 
   it('should create', () => {
     fixture.detectChanges();
 
-    expect(component).toBeTruthy();
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+
+  it('should show', () => {
+    fixture.detectChanges();
+
+    expect(pageObject.titleText).toBe('Обработка данных');
+    expect(pageObject.contentText).toBe('Пожалуйста, подождите ...');
+    expect(pageObject.card).toBeTruthy();
+    expect(pageObject.actions).toBeTruthy();
+    expect(pageObject.submit).toBeTruthy();
+    expect(pageObject.spinner).toBeNull();
   });
 });
