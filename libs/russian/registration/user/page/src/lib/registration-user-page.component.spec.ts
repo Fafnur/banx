9771ef1e014521ede43ed02c866d23f05da0aa8e
Aleksandr Serenko <fafnur@yandex.ru>
2022-01-03 22:54:38 +0700

    [Registration][Tests] Adding registration user page

--- libs/russian/registration/user/page/src/lib/registration-user-page.component.spec.ts
+++ libs/russian/registration/user/page/src/lib/registration-user-page.component.spec.ts
@@ -1,25 +1,70 @@
+import { CommonModule } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockModule } from 'ng-mocks';
+import { ReplaySubject } from 'rxjs';
+import { mock, when } from 'ts-mockito';
+
+import { providerOf } from '@banx/core/testing';
+import { RegistrationCardModule } from '@banx/registration/ui/card';
+import { RegistrationUserFacade } from '@banx/registration/user/state';
+import { RegistrationStepErrorModule } from '@banx/russian/registration/ui/step-error';
+import { SpinnerModule } from '@banx/ui/spinner';
 
 import { RegistrationUserPageComponent } from './registration-user-page.component';
+import { RegistrationUserPageComponentPo } from './registration-user-page.component.po';
 
-describe('RegistrationSocialPageComponent', () => {
-  let component: RegistrationUserPageComponent;
+describe('RegistrationUserPageComponent', () => {
+  let pageObject: RegistrationUserPageComponentPo;
   let fixture: ComponentFixture<RegistrationUserPageComponent>;
+  let registrationUserFacadeMock: RegistrationUserFacade;
+  let createUserFailure$: ReplaySubject<HttpErrorResponse>;
+
+  beforeEach(() => {
+    registrationUserFacadeMock = mock(RegistrationUserFacade);
+    createUserFailure$ = new ReplaySubject<HttpErrorResponse>(1);
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        NoopAnimationsModule,
+        RouterTestingModule,
+        SpinnerModule,
+        MockModule(MatButtonModule),
+        MockModule(RegistrationCardModule),
+        MockModule(RegistrationStepErrorModule),
+      ],
       declarations: [RegistrationUserPageComponent],
+      providers: [providerOf(RegistrationUserFacade, registrationUserFacadeMock)],
     }).compileComponents();
   });
 
   beforeEach(() => {
+    when(registrationUserFacadeMock.createUserFailure$).thenReturn(createUserFailure$);
+
     fixture = TestBed.createComponent(RegistrationUserPageComponent);
-    component = fixture.componentInstance;
+    pageObject = new RegistrationUserPageComponentPo(fixture);
   });
 
   it('should create', () => {
     fixture.detectChanges();
 
-    expect(component).toBeTruthy();
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+
+  it('should show', () => {
+    fixture.detectChanges();
+
+    expect(pageObject.titleText).toBe('Обработка данных');
+    expect(pageObject.contentText).toBe('Пожалуйста, подождите ...');
+    expect(pageObject.card).toBeTruthy();
+    expect(pageObject.actions).toBeTruthy();
+    expect(pageObject.submit).toBeTruthy();
+    expect(pageObject.spinner).toBeNull();
   });
 });
