e4c36259e98e04f498d86a19f5127e16a5bc6d6e
Aleksandr Serenko <fafnur@yandex.ru>
2022-01-03 16:24:28 +0700

    [Registration][Tests] Starting create tests

--- libs/russian/registration/conversion/page/src/lib/registration-conversion-page.component.spec.ts
+++ libs/russian/registration/conversion/page/src/lib/registration-conversion-page.component.spec.ts
@@ -1,25 +1,57 @@
+import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockModule } from 'ng-mocks';
+
+import { RegistrationConversionStateModule } from '@banx/registration/conversion/state';
+import { RegistrationCardModule } from '@banx/registration/ui/card';
+import { RegistrationStepErrorModule } from '@banx/russian/registration/ui/step-error';
+import { SpinnerModule } from '@banx/ui/spinner';
 
 import { RegistrationConversionPageComponent } from './registration-conversion-page.component';
+import { RegistrationConversionComponentPo } from './registration-conversion-page.component.po';
 
 describe('RegistrationSocialPageComponent', () => {
-  let component: RegistrationConversionPageComponent;
+  let pageObject: RegistrationConversionComponentPo;
   let fixture: ComponentFixture<RegistrationConversionPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        NoopAnimationsModule,
+        RouterTestingModule,
+        SpinnerModule,
+        MockModule(MatButtonModule),
+        MockModule(RegistrationCardModule),
+        MockModule(RegistrationConversionStateModule),
+        MockModule(RegistrationStepErrorModule),
+      ],
       declarations: [RegistrationConversionPageComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RegistrationConversionPageComponent);
-    component = fixture.componentInstance;
+    pageObject = new RegistrationConversionComponentPo(fixture);
   });
 
   it('should create', () => {
     fixture.detectChanges();
 
-    expect(component).toBeTruthy();
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+
+  it('should show', () => {
+    fixture.detectChanges();
+
+    expect(pageObject.titleText).toBe('Обработка данных');
+    expect(pageObject.contentText).toBe('Пожалуйста, подождите ...');
+    expect(pageObject.card).toBeTruthy();
+    expect(pageObject.actions).toBeTruthy();
+    expect(pageObject.submit).toBeTruthy();
+    expect(pageObject.spinner).toBeTruthy();
   });
 });
