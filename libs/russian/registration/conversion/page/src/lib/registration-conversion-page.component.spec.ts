0246753ae03cd8d10a0bf9dd9bb175bb61ac3fb0
Aleksandr Serenko <fafnur@yandex.ru>
2022-01-03 16:50:02 +0700

    [Registration][Tests] Adding tests for registration conversion page

--- libs/russian/registration/conversion/page/src/lib/registration-conversion-page.component.spec.ts
+++ libs/russian/registration/conversion/page/src/lib/registration-conversion-page.component.spec.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockModule } from 'ng-mocks';
+import { ReplaySubject } from 'rxjs';
+import { mock, when } from 'ts-mockito';
 
-import { RegistrationConversionStateModule } from '@banx/registration/conversion/state';
+import { providerOf } from '@banx/core/testing';
+import { RegistrationConversion } from '@banx/registration/conversion/common';
+import { RegistrationConversionFacade, RegistrationConversionStateModule } from '@banx/registration/conversion/state';
 import { RegistrationCardModule } from '@banx/registration/ui/card';
 import { RegistrationStepErrorModule } from '@banx/russian/registration/ui/step-error';
 import { SpinnerModule } from '@banx/ui/spinner';
@@ -13,9 +18,20 @@ import { SpinnerModule } from '@banx/ui/spinner';
 import { RegistrationConversionPageComponent } from './registration-conversion-page.component';
 import { RegistrationConversionComponentPo } from './registration-conversion-page.component.po';
 
-describe('RegistrationSocialPageComponent', () => {
+describe('RegistrationConversionComponent', () => {
   let pageObject: RegistrationConversionComponentPo;
   let fixture: ComponentFixture<RegistrationConversionPageComponent>;
+  let registrationConversionFacadeMock: RegistrationConversionFacade;
+  let completeConversionFailure$: ReplaySubject<HttpErrorResponse>;
+  let loadConversionFailure$: ReplaySubject<HttpErrorResponse>;
+  let loadConversionSuccess$: ReplaySubject<RegistrationConversion>;
+
+  beforeEach(() => {
+    registrationConversionFacadeMock = mock(RegistrationConversionFacade);
+    completeConversionFailure$ = new ReplaySubject<HttpErrorResponse>(1);
+    loadConversionFailure$ = new ReplaySubject<HttpErrorResponse>(1);
+    loadConversionSuccess$ = new ReplaySubject<RegistrationConversion>(1);
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -30,10 +46,15 @@ describe('RegistrationSocialPageComponent', () => {
         MockModule(RegistrationStepErrorModule),
       ],
       declarations: [RegistrationConversionPageComponent],
+      providers: [providerOf(RegistrationConversionFacade, registrationConversionFacadeMock)],
     }).compileComponents();
   });
 
   beforeEach(() => {
+    when(registrationConversionFacadeMock.loadConversionFailure$).thenReturn(loadConversionFailure$);
+    when(registrationConversionFacadeMock.loadConversionSuccess$).thenReturn(loadConversionSuccess$);
+    when(registrationConversionFacadeMock.completeConversionFailure$).thenReturn(completeConversionFailure$);
+
     fixture = TestBed.createComponent(RegistrationConversionPageComponent);
     pageObject = new RegistrationConversionComponentPo(fixture);
   });
@@ -52,6 +73,6 @@ describe('RegistrationSocialPageComponent', () => {
     expect(pageObject.card).toBeTruthy();
     expect(pageObject.actions).toBeTruthy();
     expect(pageObject.submit).toBeTruthy();
-    expect(pageObject.spinner).toBeTruthy();
+    expect(pageObject.spinner).toBeNull();
   });
 });
