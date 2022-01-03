1c1558b2d51c473c0e523266bd4391e07eb74c80
Aleksandr Serenko <fafnur@yandex.ru>
2022-01-03 22:36:42 +0700

    [Registration][Tests] Adding tests for registration finish page

--- libs/russian/registration/finish/page/src/lib/registration-finish-page.component.spec.ts
+++ libs/russian/registration/finish/page/src/lib/registration-finish-page.component.spec.ts
@@ -1,25 +1,70 @@
+import { CommonModule } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockModule } from 'ng-mocks';
+import { ReplaySubject } from 'rxjs';
+import { mock, when } from 'ts-mockito';
+
+import { providerOf } from '@banx/core/testing';
+import { RegistrationFinishFacade } from '@banx/registration/finish/state';
+import { RegistrationCardModule } from '@banx/registration/ui/card';
+import { RegistrationStepErrorModule } from '@banx/russian/registration/ui/step-error';
+import { SpinnerModule } from '@banx/ui/spinner';
 
 import { RegistrationFinishPageComponent } from './registration-finish-page.component';
+import { RegistrationFinishPageComponentPo } from './registration-finish-page.component.po';
 
-describe('RegistrationSocialPageComponent', () => {
-  let component: RegistrationFinishPageComponent;
+describe('RegistrationFinishPageComponent', () => {
+  let pageObject: RegistrationFinishPageComponentPo;
   let fixture: ComponentFixture<RegistrationFinishPageComponent>;
+  let registrationFinishFacadeMock: RegistrationFinishFacade;
+  let finishRegistrationFailure$: ReplaySubject<HttpErrorResponse>;
+
+  beforeEach(() => {
+    registrationFinishFacadeMock = mock(RegistrationFinishFacade);
+    finishRegistrationFailure$ = new ReplaySubject<HttpErrorResponse>(1);
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        NoopAnimationsModule,
+        RouterTestingModule,
+        SpinnerModule,
+        MockModule(MatButtonModule),
+        MockModule(RegistrationCardModule),
+        MockModule(RegistrationStepErrorModule),
+      ],
       declarations: [RegistrationFinishPageComponent],
+      providers: [providerOf(RegistrationFinishFacade, registrationFinishFacadeMock)],
     }).compileComponents();
   });
 
   beforeEach(() => {
+    when(registrationFinishFacadeMock.finishRegistrationFailure$).thenReturn(finishRegistrationFailure$);
+
     fixture = TestBed.createComponent(RegistrationFinishPageComponent);
-    component = fixture.componentInstance;
+    pageObject = new RegistrationFinishPageComponentPo(fixture);
   });
 
   it('should create', () => {
     fixture.detectChanges();
 
-    expect(component).toBeTruthy();
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+
+  it('should show', () => {
+    fixture.detectChanges();
+
+    expect(pageObject.titleText).toBe('Обработка данных');
+    expect(pageObject.contentText).toBe('Пожалуйста, подождите ...');
+    expect(pageObject.card).toBeTruthy();
+    expect(pageObject.actions).toBeTruthy();
+    expect(pageObject.submit).toBeTruthy();
+    expect(pageObject.spinner).toBeNull();
   });
 });
