372ddf1fd173e790e9d8191e55e54600cc7e3711
Aleksandr Serenko <fafnur@yandex.ru>
2022-01-03 22:46:12 +0700

    [Registration][Tests] Adding registration step error

--- libs/russian/registration/ui/step-error/src/lib/registration-step-error.component.spec.ts
+++ libs/russian/registration/ui/step-error/src/lib/registration-step-error.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockModule } from 'ng-mocks';
+
+import { PATHS_STUB } from '@banx/core/navigation/common';
+import { NavigationSharedModule } from '@banx/core/navigation/shared';
 
 import { RegistrationStepErrorComponent } from './registration-step-error.component';
 
@@ -8,7 +13,9 @@ describe('RegistrationStepErrorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, MockModule(NavigationSharedModule)],
       declarations: [RegistrationStepErrorComponent],
+      providers: [PATHS_STUB],
     }).compileComponents();
   });
 
